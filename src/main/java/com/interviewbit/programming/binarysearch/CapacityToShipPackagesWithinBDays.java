ff7019722cc63a1ea9326d90d0a844fbf7122878
Dongmin Nam <dmut7691@gmail.com>
2025-01-19 01:29:01 +0900

    remove error

--- src/main/java/com/interviewbit/programming/binarysearch/CapacityToShipPackagesWithinBDays.java
+++ src/main/java/com/interviewbit/programming/binarysearch/CapacityToShipPackagesWithinBDays.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 
 public class CapacityToShipPackagesWithinBDays {
     public int solve(ArrayList<Integer> conveyorBelt, int givenDays) {
-        long[] subtotals = new long[conveyorBelt.size()];
-
-        long sum = 0L;
-        int maxWeight = 0;
-        for (int i = 0; i < conveyorBelt.size(); i++) {
-            int weight = conveyorBelt.get(i);
-
-            sum += weight;
-            subtotals[i] = sum;
-
-            maxWeight = Math.max(maxWeight, weight);
-        }
+        throw new RuntimeException("Not yet implemented.");
+//        long[] subtotals = new long[conveyorBelt.size()];
+//
+//        long sum = 0L;
+//        int maxWeight = 0;
+//        for (int i = 0; i < conveyorBelt.size(); i++) {
+//            int weight = conveyorBelt.get(i);
+//
+//            sum += weight;
+//            subtotals[i] = sum;
+//
+//            maxWeight = Math.max(maxWeight, weight);
+//        }
     }
 }
